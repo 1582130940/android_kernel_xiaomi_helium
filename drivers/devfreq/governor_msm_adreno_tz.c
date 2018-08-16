6016f27a0cefd900415ea1ddfe22be2dd46ef59a
1582130940 <1582130940@qq.com>
2018-08-16 16:39:25 +0800

    devfreq: remove adreno idler

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -316,10 +316,6 @@ static int tz_init(struct devfreq_msm_adreno_tz_data *priv,
 	return ret;
 }
 
-#ifdef CONFIG_ADRENO_IDLER
-extern int adreno_idler(struct devfreq_dev_status stats, struct devfreq *devfreq,
-		 unsigned long *freq);
-#endif
 static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 				u32 *flag)
 {
@@ -337,12 +333,6 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 		return result;
 	}
 
-	/* Prevent overflow */
-	if (stats.busy_time >= (1 << 24) || stats.total_time >= (1 << 24)) {
-		stats.busy_time >>= 7;
-		stats.total_time >>= 7;
-	}
-
 	*freq = stats.current_frequency;
 	*flag = 0;
 
@@ -354,13 +344,6 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 		*freq = devfreq->profile->freq_table[devfreq->profile->max_state - 1];
 		return 0;
 	}
-
-#ifdef CONFIG_ADRENO_IDLER
-	if (adreno_idler(stats, devfreq, freq)) {
-		/* adreno_idler has asked to bail out now */
-		return 0;
-	}
-#endif
 
 	priv->bin.total_time += stats.total_time;
 #if 1
@@ -654,7 +637,6 @@ static void do_partner_resume_event(struct work_struct *work)
 {
 	_do_partner_event(work, DEVFREQ_GOV_RESUME);
 }
-
 
 static struct devfreq_governor msm_adreno_tz = {
 	.name = "msm-adreno-tz",
